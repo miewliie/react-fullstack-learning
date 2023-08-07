ace2d11202e14778bb2125670e89c7abe0d0c5e8
miewliie <miewliie@gmail.com>
2023-08-07 12:29:17 -0700

    Add Posts model (post table structure)

---
+++ server/models/Posts.js
@@ -0,0 +1,20 @@
+module.exports = (sequelize, DataTypes) => {
+
+    // Define table = Posts
+    const Posts = sequelize.define("Posts", {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        postText: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    })
+
+    return Posts
+}
