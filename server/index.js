286e51039bc2eebbce587c6c755282344f374683
miewliie <miewliie@gmail.com>
2023-08-07 12:28:47 -0700

    Add index for server

--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
 
-app.listen(3001, () => {
-    console.log("Server running on port 3001");
+const db = require("./models");
+
+// Routers
+const postRouter = require('./routes/Posts');
+app.use("/posts", postRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("Server running on port 3001");
+    });
 });
 
