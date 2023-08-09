13dd56abaf2f84c6e9cd573ab5d706912fdea515
miewliie <miewliie@gmail.com>
2023-08-09 14:58:09 -0700

    [client] navigate to post from home

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,8 +1,10 @@
 import axious from "axios";
 import { useEffect, useState } from 'react';
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     const [listOfPosts, setListOfPosts] = useState([]);
+    let navigate = useNavigate();
 
     useEffect(() => {
       axious.get("http://localhost:3001/posts").then((response) =>{
@@ -14,7 +16,7 @@ function Home() {
     <div>
         {listOfPosts.map((value, key) => {
         return (
-          <div className='post'> 
+          <div className='post' onClick={() => {navigate(`/post/${value.id}`)}}> 
             <div className='title'> { value.title } </div>
             <div className='body'> { value.postText } </div>
             <div className='footer'> { value.username } </div>
