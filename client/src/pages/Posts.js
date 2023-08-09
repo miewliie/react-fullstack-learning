d492476aeb9ede2aeb0507d842ad23ddba4293a7
miewliie <miewliie@gmail.com>
2023-08-09 15:23:24 -0700

    [client] add post details

--- client/src/pages/Posts.js
+++ client/src/pages/Posts.js
@@ -1,10 +1,30 @@
 import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Posts() {
     let { id } = useParams();
+    const [postObject, setPostObject] = useState({})
+
+    useEffect(() => {
+        axios.get(`http://localhost:3001/posts/byId/${id}`).then((response) =>{
+            setPostObject(response.data)
+        });
+    });
+
   return (
-    <div>
-      Post: {id}
+    <div className="postPage">
+      <div className="leftSide">
+      <div className="post" id="individual">
+          <div className="title"> {postObject.title} </div>
+          <div className="body">{postObject.postText}</div>
+          <div className="footer">{postObject.username}</div>
+        </div>
+      </div>
+
+      <div className="rightSide">
+            Comment Section
+      </div>
     </div>
   )
 }
